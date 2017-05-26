c66170ffa747ed3eceba91397a94bc87e473c6ae
joshuastagner <joshuastagner@gmail.com>
2017-05-26 16:45:24 -0700

    Modify knex production connection params to meet heroku settings

--- knexfile.js
+++ knexfile.js
@@ -20,13 +20,7 @@ module.exports = {
   },
   'production': {
     'client': 'postgresql',
-    'connection' : {
-      'database': process.env.DB_DATABASE,
-      'user': process.env.DB_USER,
-      'password': process.env.DB_PASSWORD,      
-      'host': process.env.DB_HOST,
-      'port': 5432 
-    },
+    'connection' : process.env.DATABASE_URL,
     'pool': {
       'min': 1,
       'max': 10
