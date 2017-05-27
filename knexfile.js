d057c31c5fee1acc201b4927ee8c800b7d6c031f
joshuastagner <joshuastagner@gmail.com>
2017-05-27 09:55:42 -0700

    Add production configs

--- knexfile.js
+++ knexfile.js
@@ -48,4 +48,21 @@ if (process.env.NODE_ENV === 'test') {
   };
 }
 
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds/test'
+    }
+  };
+}
 
