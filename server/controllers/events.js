c13a471172eb97bfc8857d6d0555f0f62965fcb3
Aaron Xavier <aarondxavier@gmail.com>
2017-05-31 17:25:50 -0700

    Now saves the Event to the DB.

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,8 +1,16 @@
 const models = require('../../db/models');
 
 module.exports.create = (req, res) => {
-  console.log(req.session);
   console.log(req.body);
+  models.Event.forge({title: req.body.eventName, creator_id: req.session.passport.user})
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(200).send(result);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
   //first create the event and get the event ID
 
   //then create the recipient.
@@ -21,5 +29,4 @@ module.exports.create = (req, res) => {
   //     }
   //     res.status(500).send(err);
   //   });
-  res.send();
 };
