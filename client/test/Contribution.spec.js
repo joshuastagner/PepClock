14949c4ced0a0acacca37db026a6a41c3a77e4ec
Alex Matranga <amatranga892@gmail.com>
2017-05-31 20:02:06 -0700

    Implement Contribution tests

---
+++ client/test/Contribution.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Contribution from '../src/components/Contribution.jsx';
+
+describe('Contribution component', function(){
+  const wrapper = shallow(<Contribution />);
+  it('contains the correct class', function(){
+    
+  })
+})
