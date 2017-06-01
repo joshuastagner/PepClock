8194e0b3ebbf0491bdffea62c5539907f545d728
Alex Matranga <amatranga892@gmail.com>
2017-05-31 17:21:05 -0700

    Refactor tests to use shallow instead of mount

--- client/test/Contributions.spec.js
+++ client/test/Contributions.spec.js
@@ -5,12 +5,15 @@ import Create from '../src/components/Contributions.jsx';
 
 describe('Contributions Component', function(){
   it('contains the correct class', function(){
-    expect(mount(<Contributions />).find('.contribution').length).to.equal(1);
+    expect(shallow(<Contributions />).find('.contribution').length).to.equal(1);
   });
   it('contains the event title', function(){
-    expect(mount(<Contributions />).find('.title').length).to.equal(1);
+    expect(shallow(<Contributions />).find('.title').length).to.equal(1);
   });
   it('contains all contributions from all other users', function(){
-    expect(mount(<Contributions />).find('.user-contributions').length).to.equal(1);
+    expect(shallow(<Contributions />).find('.user-contributions').length).to.equal(1);
+  });
+  xit('contains a comment form', function(){
+    expect(shallow(<Contributions />).find('.comment').length).to.equal(1);
   });
 });
