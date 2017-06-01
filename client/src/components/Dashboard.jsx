14ca1d27843f6b2afd6bce27735b974ff45e2a27
Eric Pan <epan@users.noreply.github.com>
2017-06-01 11:46:54 -0700

    (feat) Render dashboard with fixture events in state

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import EventList from './EventList';
+// TODO: Remove event fixture after replacing with real data
+import eventFixture from '../../test/fixtures/event_list';
 
-const Dashboard = () => (
-  <div>
-    <h1>Dashboard</h1>
-    <Link to="/create">Create event</Link>
-    <ul>
-      <li><Link to="/events/1">Event 1</Link></li>
-      <li><Link to="/events/2">Event 2</Link></li>
-      <li><Link to="/events/3">Event 3</Link></li>
-    </ul>
-  </div>
-);
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+  }
+
+  componentDidMount () {
+    // TODO: Fetch events for this user from DB
+    this.setState({
+      events: eventFixture
+    });
+  }
+
+  render () {
+    console.log(this.state.events);
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <Link to="/create">Create event</Link>
+        {/* <EventList /> */}
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
---
+++ client/test/fixtures/event_list.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'Fare thee well',
+    deliver_at: '2017-10-10 14:00'
+  },
+  {
+    id: 2,
+    title: 'Thank you Magee',
+    deliver_at: '2017-10-10 14:00'
+  },
+  {
+    id: 3,
+    title: 'More cowbell',
+    deliver_at: '2017-10-10 14:00'
+  }
+];
