13d2d971c61cbe1f9b3a76f07dd72af55f437b3b
Alex Matranga <amatranga892@gmail.com>
2017-06-01 19:37:56 -0700

    Now saves contributions to the DB
    Without hard-coded user_id

--- server/controllers/contributions.js
+++ server/controllers/contributions.js
@@ -1,19 +1,22 @@
 const models = require('../../db/models');
 
 module.exports.create = (req, res) => {
-  console.log(req.body);
-  models.Contribution.forge({
-    text: req.body.contributionText,
-    type: 'message',
-    contributor_id: 1
-  })
-  .save()
-  .then(result => {
-    console.log(result);
-    res.status(200).send(result);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).send(err);
+  // console.log(req.body);
+    models.Contributor.where({user_id: req.session.passport.user, event_id: req.body.eventId}).fetch().then(function(result){
+      console.log(result);
+      models.Contribution.forge({
+      text: req.body.contributionText,
+      type: 'message',
+      contributor_id: result.id
+    })
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(200).send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
   });
 }
