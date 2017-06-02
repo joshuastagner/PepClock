08f3737d9bef4c642e9bdf01583add3010cb2692
joshuastagner <joshuastagner@gmail.com>
2017-06-01 19:30:56 -0700

    Stub out send.invite worker and pseudo-code worker

--- workers/send.invites.js
+++ workers/send.invites.js
@@ -1,2 +1,20 @@
 const email = require('./utils/email');
 
+
+class InviteWorker {
+  work () {
+    // Find all invitations where sent is false
+
+      // for each invitation generate link for event
+        // store link for easy fetching
+
+      // send emails
+      
+      // on success update invitation sent to true
+   
+  }
+}
+
+worker = new InviteWorker;
+
+worker.work();
