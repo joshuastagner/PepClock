e23ec922d6303d206252d031be6e4da96aad5536
joshuastagner <joshuastagner@gmail.com>
2017-06-01 17:05:06 -0700

    Add migration with updates to invitations and events tables

---
+++ db/migrations/20170601160250_update_events_invitations.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('invitations', function(table) {
+      table.boolean('sent').notNullable();
+    }),
+
+    knex.schema.table('events', function(table) {
+      table.dateTime('delivery_time').notNullable().defaultTo('2000-01-01 23:00:00');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('invitations', function(table) {
+      table.dropColumn('sent');
+    }),
+
+    knex.schema.table('events', function(table) {
+      table.dropColumn('delivery_time');
+    })
+  ]);
+};
