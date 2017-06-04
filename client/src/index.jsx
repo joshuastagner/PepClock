7c0d0e36c389111e8046be045a246f5f77f1de9f
joshuastagner <joshuastagner@gmail.com>
2017-06-04 15:45:09 -0700

    Remove unused require

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
-import LoginControl from './components/LoginControl';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
