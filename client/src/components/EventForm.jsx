944a189a701e20bea5b722acb375a2f1a79d90e7
Eric Pan <epan@users.noreply.github.com>
2017-06-06 15:20:10 -0700

    (refactor) Clean up bootstrap on create form

--- client/src/components/EventForm.jsx
+++ client/src/components/EventForm.jsx
@@ -12,49 +12,63 @@ class EventForm extends React.Component {
   render() {
     return (
       <div className="col-xs-12">
-        <div className="row">
 
-          <form className="col-12">
-            <div className="form-group col-lg-7">
+        <div className="row">
+          <form className="col-lg-7">
+            <div className="form-group">
               <label>Name your event</label>
               <input type="text" className="form-control" name="eventName" placeholder="e.g. Happy Birthday Lisa!" onChange={this.props.handleChange}/>
             </div>
-            <div className="form-group col-lg-7">
+            <div className="form-group">
               <label>Recipient's first name</label>
               <input type="text" className="form-control" name="firstName" placeholder="Lisa" onChange={this.props.handleChange}/>
             </div>
-            <div className="form-group col-lg-7">
+            <div className="form-group">
               <label>Recipient's last name</label>
               <input type="text" className="form-control" name="lastName" placeholder="Johnson" onChange={this.props.handleChange}/>
             </div>
-            <div className="form-group col-lg-7">
+            <div className="form-group">
               <label>Recipient's email</label>
               <input type="email" className="form-control" name="email" placeholder="lisa@gmail.com" onChange={this.props.handleChange}/>
             </div>
           </form>
         </div>
 
-        <div className="row" style={{marginBottom: '15px'}}>
+        <div className="row">
           <div className="col-lg-7">
-            <DateTimeField minDate={moment().subtract(1, 'days')} defaultText="Select the event date and time" onChange={this.props.handleDateChange}/>
-          </div>
-          <div className="row" style={{marginBottom: '15px'}}>
-            <div className="col-lg-7">
-            <label>Invite others by email</label>
-            <ReactTags tags={this.props.tags}
-              handleDelete={this.props.handleDelete}
-              handleAddition={this.props.handleAddition}
-              placeholder="Enter email or paste a list separated by commas"
-              maxLength="50"
-              delimiters={[13, 44]}
-              classNames={{tagInputField: 'form-control input-md', tag: 'btn btn-info', remove: ''}}/>
+            <div className="form-group">
+              <label>Delivery date and time</label>
+              <DateTimeField
+                minDate={moment().subtract(1, 'days')}
+                defaultText="Select the event date and time"
+                onChange={this.props.handleDateChange}
+              />
             </div>
           </div>
         </div>
 
-        <button type="button" className="btn btn-primary" onClick={this.props.handleSubmit}>Create your event!</button>
+        <div className="row">
+          <div className="col-lg-7">
+            <div className="form-group">
+              <label>Invite others by email</label>
+              <ReactTags tags={this.props.tags}
+                handleDelete={this.props.handleDelete}
+                handleAddition={this.props.handleAddition}
+                placeholder="Enter email or paste a list separated by commas"
+                maxLength="50"
+                delimiters={[13, 44]}
+                classNames={{tagInputField: 'form-control input-md', tag: 'btn btn-info', remove: ''}}
+              />
+            </div>
+          </div>
+        </div>
 
-    </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={this.props.handleSubmit}>Create your event!</button>
+
+      </div>
     );
   }
 }
