0a52ca0ae43ae5277ae20f43b5b4dfd8ab5519fd
Eric Pan <epan@users.noreply.github.com>
2017-06-06 14:02:56 -0700

    (feat) Fix extra bootstrap containers

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -21,17 +21,15 @@ const Routes = () => (
   <div>
     <Nav />
     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PrivateRoute path="/dashboard" component={Dashboard} />
-          <PrivateRoute path="/create" component={Create} />
-          <PrivateRoute path="/edit/:id" component={Create} />
-          <PrivateRoute path="/events/:id" component={Event} />
-          <Route path="/redirected" component={Redirected} />
-          <Route render={ () => <h1>404 Page Not Found</h1>} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/create" component={Create} />
+        <PrivateRoute path="/edit/:id" component={Create} />
+        <PrivateRoute path="/events/:id" component={Event} />
+        <Route path="/redirected" component={Redirected} />
+        <Route render={ () => <h1>404 Page Not Found</h1>} />
+      </Switch>
     </Router>
   </div>
 );
