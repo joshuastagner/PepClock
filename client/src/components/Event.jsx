733d79e8e907c82f0c4455f8599cbf4a326b08d5
amatranga <amatranga892@gmail.com>
2017-06-05 18:58:20 -0700

    Can I commit now?

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ContributionList from './ContributionList';
 import axios from 'axios';
+import moment from 'moment';
 
 
 class Event extends React.Component {
@@ -12,7 +13,11 @@ class Event extends React.Component {
       title: '',
       contributionList: [],
       contributionText: '',
-      hasPermissionToView: null
+      hasPermissionToView: null,
+      delivery_time: '',
+      curSecond: '',
+      curMinute: '',
+      curHour: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -46,6 +51,19 @@ class Event extends React.Component {
     });
   }
 
+  setDate() {
+    const now = moment();
+    const seconds = moment().second();
+    const minutes = moment().minute();
+    const hours = moment().hour();
+
+    this.setState({
+      curSecond: seconds,
+      curMinute: minutes,
+      curHour: hours
+    });
+  }
+
   checkIfContributor () {
     axios.get('/api/events/user')
       .then(({ data: events }) => {
@@ -70,13 +88,16 @@ class Event extends React.Component {
 
   getEventContent () {
     axios.get(`/api/events/${this.state.eventId}`)
-    .then(({ data: { title } }) => {
-      this.setState({ title });
+    .then(({ data: { title, delivery_time } }) => {
+      this.setState({ title, delivery_time });
       this.updateContributions();
     })
     .catch(error => {
       console.log('Error in Event data query', error);
-    });
+    })
+    .then(
+      setInterval(this.setDate.bind(this), 1000)
+    );
   }
 
   render() {
@@ -88,11 +109,23 @@ class Event extends React.Component {
 
     if (this.state.hasPermissionToView) {
       const { id } = this.props.match.params;
-      const { title, description } = this.state;
+      const { title, description, delivery_time } = this.state;
+
+      let LaunchTimeLen = moment(delivery_time).length;
+      let launchTime = moment(this.state.delivery_time).format('YYYY MM DD hh mm ss');
+      let launchTimeDisplay = moment(this.state.delivery_time).format('MMM Do YYYY || hh : mm');
+      let timeOfDay = moment(this.state.delivery_time).format('H') > 12 ? 'PM' : 'AM';
+      let launchDisplay = launchTimeDisplay + '' + timeOfDay;
+      let rightNow = moment();
+      let timeToLaunch = rightNow.to(this.state.delivery_time);
+      let happen = timeToLaunch.includes('ago') ? 'happened' : 'happening';
+
 
       return (
         <div className="event">
           <h1>{title}</h1>
+          <h3>{happen} {timeToLaunch}</h3>
+          <h5>on {launchDisplay}</h5>
           <Link to={`/edit/${id}`}>Edit event</Link>
           <hr />
           <ContributionList contributionList={this.state.contributionList}/>
