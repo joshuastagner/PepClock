7d8474802723b0374cf1199fe7d9579c71dbd767
joshuastagner <joshuastagner@gmail.com>
2017-06-05 21:50:48 -0700

    Add parameter to event route to track invitations and update invitee to contributor

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -22,7 +22,7 @@ module.exports.create = (req, res) => {
     .tap(result => {
       let inviteList = [];
       req.body.inviteEmails.forEach(email => {
-        inviteList.push({email: email, event_id: result.id, rsvp: 0, status: 'not sent'});
+        inviteList.push({email: email, event_id: result.id, rsvp: 'false', status: 'not sent'});
       });
       let invites = collections.Invitations.forge(inviteList);
       return invites.invokeThen('save');
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,14 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const models = require('../../db/models');
 const redisClient = require('redis').createClient(process.env.REDISCLOUD_URL);
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  
-  req.session.returnTo = req.path;
+
+  req.session.returnTo = req.originalUrl;
   res.redirect('/login');
 };
 
@@ -17,8 +18,38 @@ module.exports.redirect = (req, res) => {
   res.redirect(redirect);
 };
 
-module.exports.render = (req,res) => {
+module.exports.render = (req, res) => {
   res.render('index.ejs', {user: JSON.stringify(req.user)});
+};
+
+module.exports.updateAndRender = (req, res) => {
+  if (req.query.invite) {
+    const inviteId = req.query.invite;
+    const eventId = req.params.id;
+    
+    models.Invitation.where({id: inviteId}).fetch()
+      .then((model) => {
+        if (eventId !== model.attributes.event_id) {
+          model.save('rsvp', 'true', {method: 'update'})
+            .then(() => {
+              new models.Contributor({
+                user_id: req.user.id,
+                event_id: eventId,
+                role: 'contributor'
+              }).save();
+            })
+            .then(() => {
+              let redirect = '/events/' + eventId;
+              delete req.session.returnTo;
+              res.redirect(redirect);
+            });
+        } else {
+          res.send('We are on to you!!!!');
+        }
+      });
+  } else {
+    res.render('index.ejs', {user: JSON.stringify(req.user)});
+  }
 };
 
 module.exports.session = session({
