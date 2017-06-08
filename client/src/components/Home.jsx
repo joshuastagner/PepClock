3ec8109effaf804cae207e1d4a0129fdcc88b845
Eric Pan <epan@users.noreply.github.com>
2017-06-08 16:05:13 -0700

    (refactor) Remove nonworking margin top helper

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <div className="container mt-2">
+  <div className="container">
     <div className="row">
       <div className="col-xs-12">
         <h1>Welcome to PepClock</h1>
