739df8ed7469f590fbf07faea7214cc36123074b
joshuastagner <joshuastagner@gmail.com>
2017-06-12 16:31:50 -0700

    Make buttons hit worker end points

--- client/src/components/RunWorkers.jsx
+++ client/src/components/RunWorkers.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
-const RunWorkers = () => (
-  <div>
-    <a className="btn btn-outline-info" href="/api/workers/invites">Send Participant Email</a>
-    <a className="btn btn-outline-info" href="/api/workers/recipients">Send Recipient Email</a>
-  </div>
-);
+class RunWorkers extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  sendRecipientLinks() {
+    axios.get('/api/workers/recipients')
+  }
+
+  sendInviteLinks() {
+    axios.get('/api/workers/invites')
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="btn btn-outline-info" onClick={this.sendInviteLinks}>Send Invitation Emails</button>
+        <button className="btn btn-outline-info" onClick={this.sendRecipientLinks}>Send Recipient Emails</button>
+      </div>
+    );
+  }
+};
 
 export default RunWorkers;
