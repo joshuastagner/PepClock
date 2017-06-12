b4a2d3e7f35ca02704a862f00f53b22317ef2474
amatranga <amatranga892@gmail.com>
2017-06-12 15:45:57 -0700

    Implement hidden routes to send participant and recipient emails
    /runworkers will now render a component with two buttons:
    one to call the worker to send a participant email
    one to call the worker to send a recipient email

---
+++ client/src/components/RunWorkers.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RunWorkers = () => (
+  <div>
+    <a className="btn btn-outline-info" href="/api/workers/invites">Send Participant Email</a>
+    <a className="btn btn-outline-info" href="/api/workers/recipients">Send Recipient Email</a>
+  </div>
+);
+
+export default RunWorkers;
