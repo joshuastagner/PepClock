eb0f0b8c6c30d7d83f4bab3a3ba5a79bbf861356
amatranga <amatranga892@gmail.com>
2017-06-13 11:33:52 -0700

    Splash page styling
    Removed signup from Nav. Now Located on Body
    Added a description.
    Added OAuth signup links to splash page, below signup button

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-xs-12">
-        <h1>Welcome to PepClock</h1>
-        <p>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
+  <div style={{'margin-top':'50px'}} className="container-fluid">
+    <div className="row justify-content-center">
+      <div className="col-xs-12 text-center">
+        <h1 style={{'margin-bottom':'15px'}}>Welcome to PepClock</h1>
+        <p style={{'margin-bottom':'25px'}}>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
+        <a className="btn btn-secondary" href="/signup" role="button" style={{'margin-top':'15px', 'text-color':'white'}}>Signup</a>
+        <p style={{'margin-bottom':'15px', 'margin-top':'15px'}}>Or Signup with Gmail or Facebook</p>
+        <a style={{'margin-right':'15px'}} href="/auth/facebook"><img src="/assets/fb-logo.png"></img></a>
+        <a style={{'margin-right':'15px', 'margin-left':'15px'}} href="/auth/google"><img style={{width:'29px'}} src="/assets/google-logo.png"></img></a>
       </div>
     </div>
   </div>
