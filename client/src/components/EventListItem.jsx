2d6712e962e7555d5debab5232519de5bffe3368
amatranga <amatranga892@gmail.com>
2017-06-12 19:00:56 -0700

    Remove zombie code

--- client/src/components/EventListItem.jsx
+++ client/src/components/EventListItem.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 const EventListItem = ({ event }) => {
-  console.log(event);
-  console.log(event.event);
   const { id, title, delivery_time } = event.event;
   const deliveryTime = moment(delivery_time).format('MMM Do YYYY || hh:mma');
   return (
