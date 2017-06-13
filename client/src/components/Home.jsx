a1583c1aa1e74d26611a3d448fae65fad662f17c
amatranga <amatranga892@gmail.com>
2017-06-13 14:10:37 -0700

    New style for the 2FA input page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,31 +6,38 @@ const Home = () => {
   
   if (!user) {
     return (
-      <div style={{'marginTop':'50px'}} className="container-fluid">
+      <div className="container-fluid">
         <div className="row justify-content-center">
           <div className="col-xs-12 text-center">
             <h1 style={{'marginBottom':'15px'}}>Welcome to PepClock</h1>
-            <p style={{'marginBottom':'25px'}}>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
+            <p>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
 
             <hr />
 
-            <form action="/signup" method="post" style={{"marginTop":"30px"}}>
+            <p>Signup with your email</p>
+
+            <form action="/signup" method="post" style={{"marginTop":"10px"}} className="text-left">
               <div className="form-group">
-                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} placeholder="Name" type="text" className="form-control" name="first" />
+                <label>Name</label>
+                <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="text" className="form-control" name="first" />
               </div>
               <div className="form-group">
-                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="text" placeholder="Email" className="form-control" name="email" />
+                <label>Email</label>
+                <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="text" className="form-control" name="email" />
               </div>
               <div className="form-group">
-                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="password" placeholder="Password" className="form-control" name="password" />
+                <label>Password</label>
+                <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="password" className="form-control" name="password" />
               </div>
 
               <button type="button submit" className="btn btn-primary" style={{"marginTop":"15px", "marginBottom":"30px"}}>Signup</button>
             </form>
 
-            <p style={{'marginBottom':'15px', 'marginTop':'15px'}}>Or Signup with Gmail or Facebook</p>
+            <hr />
+            <p style={{'marginBottom':'15px', 'marginTop':'15px'}}>Or Signup Facebook or Google</p>
             <a style={{'marginRight':'15px'}} href="/auth/facebook"><img src="/assets/fb-logo.png"></img></a>
             <a style={{'marginRight':'15px', 'marginLeft':'15px'}} href="/auth/google"><img style={{width:'30px'}} src="/assets/google-logo.png"></img></a>
+            <hr />
           </div>
         </div>
       </div>
