6139ccb42e70c7b397e8b75afcc9abb26adf0a25
amatranga <amatranga892@gmail.com>
2017-06-13 12:11:59 -0700

    Updated splash page to have signup form
    Signup.ejs is now not needed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = () => (
-  <div style={{'margin-top':'50px'}} className="container-fluid">
-    <div className="row justify-content-center">
-      <div className="col-xs-12 text-center">
-        <h1 style={{'margin-bottom':'15px'}}>Welcome to PepClock</h1>
-        <p style={{'margin-bottom':'25px'}}>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
-        <a className="btn btn-secondary" href="/signup" role="button" style={{'margin-top':'15px', 'text-color':'white'}}>Signup</a>
-        <p style={{'margin-bottom':'15px', 'margin-top':'15px'}}>Or Signup with Gmail or Facebook</p>
-        <a style={{'margin-right':'15px'}} href="/auth/facebook"><img src="/assets/fb-logo.png"></img></a>
-        <a style={{'margin-right':'15px', 'margin-left':'15px'}} href="/auth/google"><img style={{width:'29px'}} src="/assets/google-logo.png"></img></a>
+const Home = () => {
+  const user = window.user;
+  
+  if (!user) {
+    return (
+      <div style={{'marginTop':'50px'}} className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-xs-12 text-center">
+            <h1 style={{'marginBottom':'15px'}}>Welcome to PepClock</h1>
+            <p style={{'marginBottom':'25px'}}>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
+
+            <hr />
+
+            <form action="/signup" method="post" style={{"marginTop":"30px"}}>
+              <div class="form-group">
+                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} placeholder="Name" type="text" class="form-control" name="first" />
+              </div>
+              <div class="form-group">
+                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="text" placeholder="Email" class="form-control" name="email" />
+              </div>
+              <div class="form-group">
+                  <input style={{"marginTop":"2px", "marginBottom":"2px"}} type="password" placeholder="Password" class="form-control" name="password" />
+              </div>
+
+              <button type="button submit" className="btn btn-primary" style={{"marginTop":"15px", "marginBottom":"30px"}}>Signup</button>
+            </form>
+
+            <p style={{'marginBottom':'15px', 'marginTop':'15px'}}>Or Signup with Gmail or Facebook</p>
+            <a style={{'marginRight':'15px'}} href="/auth/facebook"><img src="/assets/fb-logo.png"></img></a>
+            <a style={{'marginRight':'15px', 'margin-left':'15px'}} href="/auth/google"><img style={{width:'30px'}} src="/assets/google-logo.png"></img></a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  } else {
+    return (
+      <div style={{'marginTop':'50px'}} className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-xs-12 text-center">
+            <h1 style={{'marginBottom':'15px'}}>Welcome to PepClock</h1>
+            <p style={{'marginBottom':'25px'}}>Collect and share words, photos, and videos of encouragement to deliver to someone later</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Home;
--- client/src/components/LoginControl.jsx
+++ client/src/components/LoginControl.jsx
@@ -12,7 +12,7 @@ const LoginControl = () => {
   } else {
     return (
         <div className="navbar-nav">
-          <a style={{"color":"#888"}} className="nav-item nav-link">Already have an account?</a>
+          <a style={{"color":"#888"}} href="/login" className="nav-item nav-link">Already have an account?</a>
           <a className="nav-item nav-link" href="/login">Login</a>
           {/*<a className="nav-item nav-link" href="/signup">Signup</a>*/}
         </div>
