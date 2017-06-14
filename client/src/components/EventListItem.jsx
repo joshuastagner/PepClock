3e0d39726997f1e3acd1afcf794265e46b3a74e6
joshuastagner <joshuastagner@gmail.com>
2017-06-14 14:49:49 -0700

    Fix error in merg

--- client/src/components/EventListItem.jsx
+++ client/src/components/EventListItem.jsx
@@ -7,7 +7,7 @@ const EventListItem = ({ event }) => {
   const deliveryTime = moment(delivery_time).format('MMM Do YYYY || hh:mma');
   return (
     <li style={{listStyle:"none"}}>
-      <Link to={`/events/${event_id}`}>{title}</Link> {`for ${first_name} ${last_name} on ${delivery_time}`}
+      <Link to={`/events/${event_id}`}>{title}</Link> {`for ${first_name} ${last_name} on ${deliveryTime}`}
       <hr />
     </li>
   );
