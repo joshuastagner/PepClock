55cc4fe4f1489ad3a5a00021622c3e40d11d9ea6
amatranga <amatranga892@gmail.com>
2017-06-14 17:19:03 -0700

    Change message above Facebook/Google login/signup

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -4,7 +4,7 @@ const OAuth = () => {
   return(
     <div className="col text-center">
       <hr />
-      <p style={{'marginBottom':'15px', 'marginTop':'15px'}}>Or Sign-in Facebook or Google</p>
+      <p style={{'marginBottom':'15px', 'marginTop':'15px'}}>Or use Facebook or Google</p>
       <a style={{'marginRight':'15px'}} href="/auth/facebook"><img src="/assets/fb-logo.png"></img></a>
       <a style={{'marginLeft':'15px'}} href="/auth/google"><img style={{width:'30px'}} src="/assets/google-logo.png"></img></a>
       <hr />
